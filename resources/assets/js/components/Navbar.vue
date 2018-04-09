7cc934ac65785a682cf4546d212523bdfd86829c
abdullahalnoor <aalnoor74@gmail.com>
2018-04-09 19:59:35 +0600

    front end vue js - crud laravel api

---
+++ resources/assets/js/components/Navbar.vue
@@ -0,0 +1,9 @@
+<template>
+<div>
+  <nav class="navbar navbar-expand-sm  bg-info mb-2">
+  <div class="container-fluid">
+    <a href="#" class="navbar-brand">Laravel APi</a>
+  </div>
+</nav>
+</div>
+</template>
